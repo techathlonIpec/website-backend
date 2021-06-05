1def8afe6cf202d9a478091f1701bd1662d65c13
Ashutosh Kumar <raj.fungus@gmail.com>
2021-06-05 21:46:24 +0530

    MailGun SMTP

--- functions/nodemailer.js
+++ functions/nodemailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.mailgun.org",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
