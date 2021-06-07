5ebb82bba24bf5bf9e02554c51ebe9eb4c51be62
Ashutosh Kumar <raj.fungus@gmail.com>
2021-06-07 13:02:21 +0530

    Excel Download

---
+++ routes/downloads.js
@@ -0,0 +1,117 @@
+let express = require('express')
+let router = express.Router()
+let exportFromJSON = require('export-from-json')
+
+// Models Import
+const hackathon = require("../models/hackathon");
+const speciawar = require("../models/speciaWar");
+const infomaze = require("../models/infomaze");
+const captureTheFlag = require("../models/captureTheFlag");
+const vividly = require("../models/vividly");
+const { route } = require('./registration');
+
+router.get('/hackathonData',(req,response)=>{
+    // PasswordCheck Here
+    let currentDate = new Date();
+    let fileName = 'hackathon'+currentDate.getDate()+'-'+currentDate.getMonth()+'time' + currentDate.getHours()+':'+currentDate.getMinutes()
+    let exportType = 'xls'
+    hackathon.find().then(foundData=>{
+        let data = JSON.stringify(foundData)
+        const result = exportFromJSON({
+            data,
+            fileName,
+            exportType,
+            processor (content, type, fileName) {
+                response.setHeader('Content-Type', 'application/vnd.ms-excel')
+                response.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                return content
+            }
+        })
+        response.send(result)
+    })
+})
+
+router.get('/ctfData',(req,response)=>{
+    // PasswordCheck Here
+    let currentDate = new Date();
+    let fileName = 'ctf'+currentDate.getDate()+'-'+currentDate.getMonth()+'time' + currentDate.getHours()+':'+currentDate.getMinutes()
+    let exportType = 'xls'
+    captureTheFlag.find().then(foundData=>{
+        let data = JSON.stringify(foundData)
+        const result = exportFromJSON({
+            data,
+            fileName,
+            exportType,
+            processor (content, type, fileName) {
+                response.setHeader('Content-Type', 'application/vnd.ms-excel')
+                response.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                return content
+            }
+        })
+        response.send(result)
+    })
+})
+
+router.get('/speciawarData',(req,response)=>{
+    // PasswordCheck Here
+    let currentDate = new Date();
+    let fileName = 'speciawar'+currentDate.getDate()+'-'+currentDate.getMonth()+'time' + currentDate.getHours()+':'+currentDate.getMinutes()
+    let exportType = 'xls'
+    speciawar.find().then(foundData=>{
+        let data = JSON.stringify(foundData)
+        const result = exportFromJSON({
+            data,
+            fileName,
+            exportType,
+            processor (content, type, fileName) {
+                response.setHeader('Content-Type', 'application/vnd.ms-excel')
+                response.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                return content
+            }
+        })
+        response.send(result)
+    })
+})
+
+router.get('/infomazeData',(req,response)=>{
+    // PasswordCheck Here
+    let currentDate = new Date();
+    let fileName = 'infomaze'+currentDate.getDate()+'-'+currentDate.getMonth()+'time' + currentDate.getHours()+':'+currentDate.getMinutes()
+    let exportType = 'xls'
+    infomaze.find().then(foundData=>{
+        let data = JSON.stringify(foundData)
+        const result = exportFromJSON({
+            data,
+            fileName,
+            exportType,
+            processor (content, type, fileName) {
+                response.setHeader('Content-Type', 'application/vnd.ms-excel')
+                response.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                return content
+            }
+        })
+        response.send(result)
+    })
+})
+
+router.get('/vividlyData',(req,response)=>{
+    // PasswordCheck Here
+    let currentDate = new Date();
+    let fileName = 'vividly'+currentDate.getDate()+'-'+currentDate.getMonth()+'time' + currentDate.getHours()+':'+currentDate.getMinutes()
+    let exportType = 'xls'
+    vividly.find().then(foundData=>{
+        let data = JSON.stringify(foundData)
+        const result = exportFromJSON({
+            data,
+            fileName,
+            exportType,
+            processor (content, type, fileName) {
+                response.setHeader('Content-Type', 'application/vnd.ms-excel')
+                response.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                return content
+            }
+        })
+        response.send(result)
+    })
+})
+module.exports = router
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const chalk = require('chalk')
 const cors = require('cors')
-// var Ddos = require('ddos')
-// var ddos = new Ddos({burst:10, limit: 10})
+var Ddos = require('ddos')
+var ddos = new Ddos({burst:10, limit: 10})
 require('dotenv').config()
+
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-// app.use(ddos.express);
+app.use(ddos.express);
 
 //Connecting Database-MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -24,11 +25,14 @@ mongoose.connect(process.env.MONGODB_URI, {
     }
 })
 
+
 // Importing Routes
 let registration = require('./routes/registration')
+let downloads = require('./routes/downloads')
 
 // using Routes
 app.use('/', registration)
+app.use('/download/', downloads)
 
 app.listen(process.env.PORT , () => {
     console.log(chalk.greenBright("Server is listening at port "+process.env.PORT))
